ee51e122114f6d02c226d5826993450213813076
PavLikh <lpa2@mail.ru>
2022-09-05 16:30:06 +0300

    add <code>

--- resources/views/task3.blade.php
+++ resources/views/task3.blade.php
@@ -17,26 +17,89 @@
         <p>Реализуем паттерн внедерение зависимости (DI). Laravel предоставляет инструмент для реализации данного паттерна - Service Container. Создадим ParseHtmlService и опишем в нем методы парсинга.</p>
     </div>
     <pre>
-        <code class="language-php">function ft_array_sort (&$arr)
+        <code class="language-php">class ParseHtmlService
 {
-    $n = count($arr) - 1;
-    while($n) {
-        $max = 0;
-        $i = $n;
-        while ($i) {
-            if($max < $arr[$i]) {
-                $max = $arr[$i];
-                array_swap($arr, $i);
-                $i = $n;
-            }
-            $i--;
+    public function start(string $html, string $method)
+    {
+        if(method_exists($this, $method)) {
+            return $this->$method($html);
         }
-        if ($max > $arr[$n]){
-            array_swap($arr, $n);
-        }
-        $n--;
     }
-}       </code>
+
+    /**
+     * @param string $html
+     * @return array
+     */
+    protected function getTegs(string $html)
+    {
+        $array = $this->countHtmlTegs($this->getAllTags($html));
+        return $array;
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    protected function countHtmlTegs(array $data)
+    {
+        $array = [];
+        $i = 0;
+        while($i < count($data)) {
+            $j = 0;
+            foreach($array as $key => $val) {
+                if ($data[$i] == $key) {
+                    $val++;
+                    $array[$key]++;
+                    break;
+                }
+                $j++;
+            }
+            if($j == count($array)) {
+                $array[$data[$i]] = 1;
+            }
+            $i++;
+        }
+        return $array;
+    }
+
+    /**
+     * @param string $html
+     * @return array
+     */
+    protected function getAllTags(string $html)
+    {
+        $array = [];
+        $i = 0;
+        $teg_nb = 0;
+        $k = 0;
+        $start = -1;
+        $end = -1;
+        while($i < strlen($html)){
+            if($html[$i] == '<' && ($html[$i+1] != ' ' && $html[$i+1] != '!' && $html[$i+1] != '/')){
+                if ($start < 0) {
+                    $i++;
+                }
+                $start = 1;
+            }
+            if($start == 1){
+                if($html[$i] != '>' && $html[$i] != ' '){
+                    $array[$teg_nb][$k++] = $html[$i];
+                }
+            }
+            if($start > 0 && ($html[$i] == '>' || $html[$i] == ' ')){
+                $start = -1;
+                $teg_nb++;
+                $k = 0;
+            }
+            $i++;
+        }
+        $data;
+        foreach($array as $val) {
+            $data[] = implode('', $val);
+        }
+        return $data;
+    }
+}</code>
     </pre>
     <div class="company-text">
         <p>Зарегистрируем связывание в соответствующем провайдере ParseHtmlServiceProvider, после чего сможем вызывать экзмепояры класса в контроллерах.</p>
